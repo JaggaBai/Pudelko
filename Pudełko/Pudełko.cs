19fb712a913ce44be5bb2839b62011c59c234742
Jagga Bai <dzagur@post.pl>
2022-09-23 18:04:13 +0200

    Add project files.

---
+++ Pudełko/Pudełko.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pudełko
+{
+    public class Pudełko
+    {
+        UnitOfMeasure miara { get; set; }
+        private double A { get; set; }
+        private double B { get; set; }
+        private double C { get; set; }
+
+        public Pudełko(double a = 0.1, double b = 0.1, double c =0.1, UnitOfMeasure u = UnitOfMeasure.meter)
+        {
+            A = a;
+            B = b;
+            C = c;
+            miara = u;
+        }
+    }
+}
