f31c863fd2cc4071762a689636bf28bbdd29d86d
Jagga Bai <dzagur@post.pl>
2022-09-24 00:44:26 +0200

    pierwsze warunki(do2)// do zmiany: warunki kolejność, lista nulla nie jest pewne, ListaPo do wrzucenia wartość w metrach next

--- Pudełko/Pudełko.cs
+++ Pudełko/Pudełko.cs
@@ -4,19 +4,41 @@ using System.Text;
 
 namespace Pudełko
 {
-    public class Pudełko
+    public sealed class Pudełko
     {
+       
         UnitOfMeasure miara { get; set; }
-        private double A { get; set; }
-        private double B { get; set; }
-        private double C { get; set; }
+        //private double A { get
+        //    set { ListaPo[0];
+        //private double B { get; set; }
+        //private double C { get; set; }
 
-        public Pudełko(double a = 0.1, double b = 0.1, double c =0.1, UnitOfMeasure u = UnitOfMeasure.meter)
+       
+        private List<double> ListaPo = new List<double>();
+
+        public Pudełko(List<double> lista = default(List<double>)/*null?*/, UnitOfMeasure u = UnitOfMeasure.meter)
         {
-            A = a;
-            B = b;
-            C = c;
             miara = u;
+            foreach (double n in lista)
+            {
+                //W przypadku próby utworzenia pudełka z którymkolwiek z parametrów niedodatnim, zgłaszany jest wyjątek ArgumentOutOfRangeException
+                if (n < 0)
+                { throw new ArgumentOutOfRangeException(); }
+                //W przypadku próby utworzenia pudełka z którymkolwiek z parametrów większym niż 10 metrów, zgłaszany jest wyjątek ArgumentOutOfRangeException.
+                else if (((u == UnitOfMeasure.milimeter) & (n > 100000)) || ((u == UnitOfMeasure.centimeter) & (n > 10000)) || ((u == UnitOfMeasure.meter) & (n > 10)))
+
+                { throw new ArgumentOutOfRangeException(); }
+            }
+           // Wszystkie parametry konstruktora są opcjonalne.
+        if (lista ==null)
+            {
+                lista = new List<double>();
+                lista.Add(10);
+                lista.Add(10);
+                lista.Add(10); //mogłoby być prościej note
+                u = UnitOfMeasure.centimeter;
+            }
+            
         }
     }
 }
