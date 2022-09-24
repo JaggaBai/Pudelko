94e9cc4e76b1824d3e03c766cc063a935dded328
Jagga Bai <dzagur@post.pl>
2022-09-24 20:18:01 +0200

    warunki do pustej/niepełnej listy dod.// metry do wrzucenia do ListyPo next

--- Pudełko/Pudełko.cs
+++ Pudełko/Pudełko.cs
@@ -6,14 +6,16 @@ namespace Pudełko
 {
     public sealed class Pudełko
     {
-       
+
         UnitOfMeasure miara { get; set; }
         //private double A { get
-        //    set { ListaPo[0];
+        //    set
+        //    {
+        //        ListaPo[0];
         //private double B { get; set; }
         //private double C { get; set; }
 
-       
+
         private List<double> ListaPo = new List<double>();
 
         public Pudełko(List<double> lista = default(List<double>)/*null?*/, UnitOfMeasure u = UnitOfMeasure.meter)
@@ -29,16 +31,78 @@ namespace Pudełko
 
                 { throw new ArgumentOutOfRangeException(); }
             }
-           // Wszystkie parametry konstruktora są opcjonalne.
-        if (lista ==null)
+            // Wszystkie parametry konstruktora są opcjonalne.
+
+            if (lista == null)
             {
                 lista = new List<double>();
                 lista.Add(10);
                 lista.Add(10);
-                lista.Add(10); //mogłoby być prościej note
+                lista.Add(10);
                 u = UnitOfMeasure.centimeter;
             }
-            
+            //Jeśli podano mniej niż 3 wartości liczbowe, pozostałe przyjmuje się jako o wartości 10 cm, ale dla ustalonej jednostki miary.
+            if (lista.Count < 3) //może być prościej ale nie wiem jak to kompetentnie zrobić/ bez błędu
+            {
+                if (lista.Count == 0)
+                { if (u == UnitOfMeasure.milimeter)
+                    {
+                        lista.Add(1000);
+                        lista.Add(1000);
+                        lista.Add(1000);
+                    }
+                    else if (u == UnitOfMeasure.centimeter)
+                    {
+                        lista.Add(10);
+                        lista.Add(10);
+                        lista.Add(10);
+                    }
+                    else
+                    {
+                        lista.Add(0.1);
+                        lista.Add(0.1);
+                        lista.Add(0.1);
+                    }
+                }
+                else if (lista.Count == 1)
+                {
+
+
+                    if (u == UnitOfMeasure.milimeter)
+                    {
+                        lista.Add(1000);
+                        lista.Add(1000);
+                    }
+                    else if (u == UnitOfMeasure.centimeter)
+                    {
+                        lista.Add(10);
+                        lista.Add(10);
+                    }
+                    else
+                    {
+                        lista.Add(0.1);
+                        lista.Add(0.1);
+                    }
+
+                }
+                else if (lista.Count == 2)
+                {
+                    if (u == UnitOfMeasure.milimeter)
+                    {
+                        lista.Add(1000);
+                    }
+                    else if (u == UnitOfMeasure.centimeter)
+                    {
+                        lista.Add(10);
+                    }
+                    else
+                    {
+                        lista.Add(0.1);
+                    }
+                }
+            }
+
+            ListaPo = lista;
         }
     }
 }
